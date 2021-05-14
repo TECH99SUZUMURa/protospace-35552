854d08bb85ec9142b5b708067d63d2ddcab4cf27
TECH99SUZUMURa <82789367+TECH99SUZUMURa@users.noreply.github.com>
2021-05-14 17:00:59 +0900

    debug2

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :set_prototype, only: [:show, :edit]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :show, :new]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
 
   def index
@@ -57,7 +57,8 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in? && @prototype.user_id == current_user.id
+    prototype = Prototype.find(params[:id])
+    unless current_user.id == prototype.user_id
       redirect_to action: :index
     end
   end
